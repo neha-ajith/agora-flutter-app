e62368cc7c1becd49411b437649bcb1d0f0a7d33
Neha Ajith <62176326+neha-ajith@users.noreply.github.com>
2020-12-17 14:30:49 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/agoraapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.agoraapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/utils/settings.dart
@@ -0,0 +1,4 @@
+/// Define App ID and Token
+const APP_ID = "4701e2e29b4b4bd3a85bc31783ee0c53";
+const Token =
+    "0064701e2e29b4b4bd3a85bc31783ee0c53IAAnGDKzPBlSynCAn9bmrZ6wbWGQwCp9j4bMIhvN4EbWmMJBJDUAAAAAEADf1vrT2mncXwEAAQDaadxf";
